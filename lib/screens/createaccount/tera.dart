49709a7b0be8e1b98cd83158a74b21518fa1c3d9
Priyanshu-Singhz <ps4151722@gmail.com>
2022-09-30 22:03:20 +0530

    minor

---
+++ lib/screens/createaccount/tera.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'interests.dart';
+
+class Terms extends StatelessWidget {
+  const Terms({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomSheet: Container(
+        decoration: BoxDecoration(color: Colors.lightBlue),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 400,
+              top: 10,
+              left: 10,
+              child: Container(
+                decoration: BoxDecoration(color: Colors.lightBlue),
+                child: Image.asset(
+                  "assets/doodle.png",
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: ContainerCard(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ContainerCard extends StatelessWidget {
+  const ContainerCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+      width: MediaQuery.of(context).size.width,
+      height: 551,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(18),
+          topRight: Radius.circular(18),
+        ),
+      ),
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Flexible(
+                  child: Text(
+                    "Let's add a profile picture !",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 3, 43, 76),
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                Flexible(
+                  child: Text(
+                    "We won’t display it publicly. We believe you don’t look your age 😉",
+                    style: TextStyle(
+                      color: Color(0xff2D4379),
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              child: Image.asset("assets/createaccount/image.png"),
+            ),
+            Spacer(),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.center,
+                  stops: [0.0, 1.0],
+                  colors: [
+                    Colors.blue.shade500,
+                    Colors.blue.shade400,
+                  ],
+                ),
+                color: Colors.deepPurple.shade300,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  minimumSize: MaterialStateProperty.all(Size(400, 50)),
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.transparent),
+                  // elevation: MaterialStateProperty.all(3),
+                  shadowColor: MaterialStateProperty.all(Colors.transparent),
+                ),
+                onPressed: () {
+                  Get.to(Interests());
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 10,
+                    bottom: 10,
+                  ),
+                  child: Text(
+                    "Next",
+                    style: TextStyle(
+                      fontSize: 18,
+                      // fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
